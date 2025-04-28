bacab968dfefda9b556396884194abe097b479ed
Anurag Sharma <anuragsharma673@gmail.com>
2025-04-28 20:44:54 +0530

    Now starting the deployment

---
+++ docker/DockerFile.ws
@@ -0,0 +1,16 @@
+FROM oven/bun:1 
+
+WORKDIR /usr/src/app
+
+COPY ./package.json /usr/src/app/
+COPY ./package-lock.json /usr/src/app/
+
+COPY . .
+
+RUN bun install 
+
+RUN bun run generate:db
+
+EXPOSE 8081
+
+CMD [ "bun","start:ws" ]
---
+++ packages/db/index.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from "@prisma/client";
+const globalForPrisma = global as unknown as { prismaClient: PrismaClient };
+
+export const prismaClient = globalForPrisma.prismaClient || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prismaClient = prismaClient;
