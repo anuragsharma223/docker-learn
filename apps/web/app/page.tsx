6a2eaaabb7836bf68975a0bf79545e101a478d88
Anurag Sharma <anuragsharma673@gmail.com>
2025-04-30 20:54:44 +0530

    CD fe

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,3 +9,4 @@ export default async function Home() {
 }
 // export const revalidate = 60; // this is the number of seconds to revalidate the page every 60 seconds because we are using server side rendering.
 export const dynamic = "force-static"; // this is to force the page to be static both of these works the same as they checkes for update in the file when there is an update in the database. 
+// not working even after doing it
