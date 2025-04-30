9897f03040c456ff27806f0a932a479a9afa41b7
Anurag Sharma <anuragsharma673@gmail.com>
2025-05-01 03:28:36 +0530

    force dynamic

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import {prismaClient} from "db/client"
-export const dynamic = "force-static"; // this is to force the page to be static both of these works the same as they checkes for update in the file when there is an update in the database. 
+export const dynamic = "force-dynamic"; // this is to force the page to be static both of these works the same as they checkes for update in the file when there is an update in the database. 
 export default async function Home() {
   const users = await prismaClient.user.findMany();
   return (
